faa41bab191d3a79298ad3621cb0c36746975d4a
Serhii Bakun <gizzmo599@gmail.com>
2022-12-07 00:06:28 +0200

    fixes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,12 +7,7 @@ import { ContactList } from 'components/ContactList/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -22,10 +17,10 @@ export class App extends Component {
   };
 
   handleSubmit = (name, number) => {
+    if (this.state.contacts.find(contact => contact.name === name)) {
+      return window.alert(`${name} is already in contacts`);
+    }
     this.setState(prevState => {
-      if (prevState.contacts.find(contact => contact.name === name)) {
-        return window.alert(`${name} is already in contacts`);
-      }
       return {
         contacts: [...prevState.contacts, { id: nanoid(), name, number }],
       };
